4b907468446a5ef290f472171391680973a82230
rahulp <rahulp@qburst.com>
2025-03-25 11:15:55 +0530

    Complete sample project

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.blue)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("I am rich")
+                    .font(.system(size: 100))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image(systemName: "diamond.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
         }
-        .padding()
+        
     }
 }
 
